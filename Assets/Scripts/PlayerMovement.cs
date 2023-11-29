3a660fa08d4f66ec859454236b7a4cc75eb20662
egmp7 <deweyerick@gmail.com>
2023-11-29 13:21:49 -0600

    player movement clean code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,38 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float movementForce;
-    [SerializeField] float sideSpeed;
-    [SerializeField] Transform cam;
-    
+    [Tooltip("Force applied for foward player movement")]
+    [SerializeField][Range(1.0f, 10.0f)] float forwardMagnitude;
+    [Tooltip("How much force applied for side player movement")]
+    [SerializeField][Range(1.0f, 10.0f)] float sideMagnitude;
+
     private Rigidbody _rb;
-    
-    void Start()
+
+    private void Start()
     {
         _rb = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
+        Move();
+    }
+
+    private void Move()
+    {
         // Inputs
         float forward = Input.GetAxisRaw("Vertical");
         float side = Input.GetAxisRaw("Horizontal");
 
-        // Camera direction
-        Vector3 camForward = cam.forward;
-        Vector3 camRight = cam.right;
-        camForward.y = 0;
-        camRight.y = 0;
-
-        // Relative Positions
-        Vector3 forwardRelative = camForward * forward;
-        Vector3 rightRelative = camRight * side * sideSpeed;
+        // Create Force
+        Vector3 forwardForce = new Vector3(0, 0, forward) * forwardMagnitude;
+        Vector3 sideForce = new Vector3(side, 0, side) * sideMagnitude;
 
         // Movement
-        Vector3 playerMovement = forwardRelative + rightRelative;
-        _rb.AddForce(playerMovement * movementForce, ForceMode.Force);
+        _rb.AddForce(forwardForce);
+        _rb.AddForce(sideForce);
     }
 }
